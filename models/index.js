9044b42085916fa26f9d35f4076f87deb62cf231
J Scott Rumptz <jscott@rumptz.tech>
2021-03-06 14:10:29 -0600

    finished model associations in model/index.js

--- models/index.js
+++ models/index.js
@@ -22,14 +22,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tagged_product',
+  as: 'tagIds',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tagged_product',
+  as: 'tagIds',
   foreignKey: 'tag_id'
 });
 
